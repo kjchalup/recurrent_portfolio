d06acb3c19211cb58b642b6ca6dff150beb0f509
Krzysztof Chalupka <kjchalupk@caltech.edu>
2017-02-04 15:51:22 -0800

    backtester now has nn training in the loop

--- linear_hf/run_backtest.py
+++ linear_hf/run_backtest.py
@@ -5,12 +5,16 @@ import joblib
 
 import neuralnet
 from preprocessing import non_nan_markets
+from batching_splitting import split_validation_training
 
 
 def myTradingSystem(DATE, OPEN, HIGH, LOW, CLOSE, VOL,CLOSE_LASTTRADE, 
     CLOSE_ASK, CLOSE_BID, RETURN, SHARE, DIVIDEND, TOTALCAP, exposure, equity, settings, fundEquity):
     all_data = np.array(OPEN)
     market_data = np.hstack([OPEN, CLOSE, HIGH, LOW])
+    print('Iter {} [{}], equity {}.'.format(settings['iter'], 
+                                            DATE[-1],
+                                            fundEquity[-1]))
 
     if settings['iter'] == 0:
         # Define a new neural net.
@@ -21,16 +25,27 @@ def myTradingSystem(DATE, OPEN, HIGH, LOW, CLOSE, VOL,CLOSE_LASTTRADE,
                                           lbd=settings['lbd'])
 
     # Train the neural net on current data.
-    # for epoch_id in range(settings['num_epochs']):
-    #     for batch_in, batch_out:
-    #         settings['nn'].train_step(
-    #             batch_in= , 
-    #             batch_out=, 
-    #             lr=settings['lr'])
+    batches_per_epoch = int(np.floor((all_data.shape[0] -
+                                      settings['horizon'] -
+                                      2 * settings['n_sharpe'] + 2)
+                                     /float(settings['batch_size'])))
+    for epoch_id in range(settings['num_epochs']):
+        for batch_id in range(batches_per_epoch):
+            _, _, all_batch, market_batch = split_validation_training(
+                all_data, market_data, valid_period=0, 
+                horizon=settings['horizon'], 
+                n_for_sharpe=settings['n_sharpe'],
+                batch_id=batch_id, 
+                batch_size=settings['batch_size'],
+                randseed=epoch_id)
+
+            settings['nn'].train_step(
+                batch_in=all_batch, 
+                batch_out=market_batch, 
+                lr=settings['lr'])
     
     # Predict a portfolio.
-    horizon = settings['n_time'] - settings['n_sharpe'] + 1
-    positions = settings['nn'].predict(all_data[-horizon:])
+    positions = settings['nn'].predict(all_data[-settings['horizon']:])
     settings['iter'] += 1
     return np.ones(OPEN.shape[1]), settings
 
@@ -40,8 +55,10 @@ def mySettings():
     # Futures Contracts
     settings['n_time'] = 20 # Use this many timesteps in one datapoint.
     settings['n_sharpe'] = 10 # This many timesteps to compute Sharpes.
+    settings['horizon'] = settings['n_time'] - settings['n_sharpe'] + 1
     settings['lbd'] = .001 # L1 regularizer strength.
     settings['num_epochs'] = 1 # Number of epochs each day.
+    settings['batch_size'] = 32
     settings['lr'] = 1e-4 # Learning rate.
     settings['iter'] = 0
     settings['lookback']=500
