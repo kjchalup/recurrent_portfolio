20496054597a4e344291a5389aa473f25c1c7a0b
alexteng <alex@cause.ai>
2017-02-16 15:11:43 -0800

    merging and pushing!

--- tests/test_runbacktest.py
+++ tests/test_runbacktest.py
@@ -1,5 +1,4 @@
 import pytest
-import quantiacsToolbox
 import numpy as np
 
 from context import linear_hf
@@ -13,34 +12,37 @@ from linear_hf import  neuralnet
 from linear_hf.costs import compute_numpy_sharpe
 from linear_hf.run_backtest import training
 def test_donttradepositions():
-    positions=np.array([1, 1, 1])
-    settings = {'markets': ['COL1','COL2','CASH']}
+    """ Test donttradeposition function returns 0 if not trading"""
+    positions = np.array([1, 1, 1])
+    settings = {'markets': ['COL1', 'COL2', 'CASH']}
     positions = dont_trade_positions(positions, settings)
     assert positions[0] == 0
     assert positions[1] == 0
     assert positions[2] == 1
 
 def test_calc_batches():
+    """ Tests calc batches"""
     settings = {'horizon': 51,
                 'val_period': 50,
                 'n_sharpe': 50,
                 'batch_size': 50}
     n_timesteps = 1000
-    
+
     batches_per_epoch = calc_batches(n_timesteps=n_timesteps, settings=settings)
-    
+
     calc = (1000 - 2 * settings['n_sharpe'] - settings['horizon'] - settings['val_period'] + 1)
     calc = calc / float(settings['batch_size'])
     calc = int(calc)
 
     assert batches_per_epoch == calc
     assert batches_per_epoch == 16
-    
+
     batches_per_epoch = calc_batches(n_timesteps=999, settings=settings)
 
     assert batches_per_epoch == calc-1
 
 def test_lr_calc():
+    """ Tests learning rate calculation is as expected"""
     settings = {'lr_mult_base': 0.1,
                 'lr': 10,
                 'num_epochs': 10}
@@ -51,8 +53,9 @@ def test_lr_calc():
     lr_new = lr_calc(settings, epoch_id=10)
     ratio = lr_new / (settings['lr'] * settings['lr_mult_base'])
     assert ratio > 0.99 and ratio < 1.01
-    
+
 def test_loss_calc():
+    "Tests loss calc is as expected"""
     np.random.rand(1)
     n_time = 20
     n_markets = 2
@@ -65,32 +68,34 @@ def test_loss_calc():
 
     data_all = np.array([np.hstack([data1, data2] *4)])
     assert data_all.shape == (n_batch, n_time, n_markets * 4)
-    
+
     n_ftrs = data_all.shape[2]
     n_time = data_all.shape[1]
     n_sharpe = n_time - 1
     n_time = n_time - 1
-    nn = neuralnet.Linear(n_ftrs, n_markets, n_time, n_sharpe)
-    settings = {'nn': nn}
-    
+    neural = neuralnet.Linear(n_ftrs, n_markets, n_time, n_sharpe)
+    settings = {'nn': neural}
+
+
     batch_in = data_all[:, :-1, :]
     batch_out = data_all[:, 1:, :]
 
     sharpe_nn = loss_calc(settings, all_batch=batch_in, market_batch=batch_out)
-    nn_pos = nn._positions_np(batch_in=batch_in)
+    nn_pos = neural._positions_np(batch_in=batch_in)
     sharpe_np = compute_numpy_sharpe(positions=nn_pos, prices=batch_out, slippage=0.05)
     ratio = sharpe_nn / sharpe_np
     assert ratio < 1.01 and ratio > 0.99
 
 def test_update_nn():
+    """ Tests update_nn"""
     n_ftrs = 4
     n_markets = 2
     n_time = 10
     n_sharpe = 3
     horizon = n_time - n_sharpe + 1
-    
-    nn = neuralnet.Linear(n_ftrs, n_markets, n_time, n_sharpe)
-    settings = {'nn': nn}
+
+    neural = neuralnet.Linear(n_ftrs, n_markets, n_time, n_sharpe)
+    settings = {'nn': neural}
     best_sharpe = 10
     epoch_sharpe = 20
     settings, best_sharpe = update_nn(settings, best_sharpe, epoch_sharpe)
@@ -105,6 +110,7 @@ def test_update_nn():
     assert best_sharpe == 20
 
 def test_training_fakedata():
+    """ Tests learning using training, should find the stock with exponential increases"""
     np.random.rand(1)
     n_time = 50
     n_markets = 3
@@ -123,8 +129,8 @@ def test_training_fakedata():
 
     n_ftrs = data_in.shape[1]
     n_time = data_all.shape[0]-20
-    nn = neuralnet.Linear(n_ftrs, n_markets, n_time, n_sharpe)
-    settings = {'nn': nn,
+    neural = neuralnet.Linear(n_ftrs, n_markets, n_time, n_sharpe)
+    settings = {'nn': neural,
                 'num_epochs': 20,
                 'n_time': n_time,
                 'n_sharpe': n_sharpe,
@@ -140,7 +146,7 @@ def test_training_fakedata():
                 'retrain_interval': 1,
                 'allow_shorting': True}
 
-    
+
     settings = training(settings, all_data=data_in, market_data=data_all)
     nn_pos = settings['nn']._positions_np(batch_in=data_in[None, :n_time, :])
     sharpe = compute_numpy_sharpe(positions=nn_pos, prices=data_all[None, :n_time, :])
@@ -148,25 +154,26 @@ def test_training_fakedata():
     assert sharpe > 1000
 
 def temp_test_training_fakedata_2():
+    """ Overfit on random data, should increase sharpe significantly"""
     np.random.rand(1)
     n_time = 50
     n_markets = 3
     n_batch = 1
     n_sharpe = 10
 
-    data1 = np.random.rand(n_time, 1) + 20
+    data1 = np.random.rand(n_time, 1) + 1
 
-    data2 = np.random.rand(n_time, 1) + 20
+    data2 = np.random.rand(n_time, 1) + 2
 
-    data3 = np.random.rand(n_time, 1) + 20
+    data3 = np.random.rand(n_time, 1) + 3
 
     data_all = np.hstack([data1, data2, data3] *4)
     data_in = np.hstack([data1, data2, data3])
     assert data_all.shape == (n_time, n_markets * 4)
     n_ftrs = data_in.shape[1]
     n_time = data_all.shape[0] - 25
-    nn = neuralnet.Linear(n_ftrs, n_markets, n_time, n_sharpe)
-    settings = {'nn': nn,
+    neural = neuralnet.Linear(n_ftrs, n_markets, n_time, n_sharpe)
+    settings = {'nn': neural,
                 'num_epochs': 100,
                 'n_time': n_time,
                 'n_sharpe': n_sharpe,
@@ -185,8 +192,8 @@ def temp_test_training_fakedata_2():
 
     horizon = settings['horizon']
     # Predict prices at time = horizon+1
-    nn_pos_b4 = np.vstack([settings['nn'].predict(data_in[i:horizon+i, :]) 
-                          for i in range(data_in.shape[0]-horizon -1)])
+    nn_pos_b4 = np.vstack([settings['nn'].predict(data_in[i:horizon+i, :])
+                           for i in range(data_in.shape[0]-horizon - 2)])
     poscheck = nn_pos_b4[None, :, :]
     pricecheck = data_all[None, horizon+1:, :]
 
@@ -196,8 +203,8 @@ def temp_test_training_fakedata_2():
     settings = training(settings, all_data=data_in, market_data=data_all)
     settings['nn'].load()
 
-    nn_pos_after = np.vstack([settings['nn'].predict(data_in[i:horizon+i, :]) 
-                             for i in range(data_in.shape[0]-horizon -1)])
+    nn_pos_after = np.vstack([settings['nn'].predict(data_in[i:horizon+i, :])
+                              for i in range(data_in.shape[0]-horizon - 2)])
 
     nn_pos_after = np.vstack(nn_pos_after)
     sharpe_after = compute_numpy_sharpe(positions=nn_pos_after[None, :, :],
