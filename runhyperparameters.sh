68769f44b4d67e9a122ca0be308e4d1492db6802
Tristan McKinney <natsirguy@gmail.com>
2017-02-21 18:39:51 -0800

    Make runhyperparameters run with an initial offset.

--- runhyperparameters.sh
+++ runhyperparameters.sh
@@ -1,6 +1,12 @@
+#!/bin/bash
+
+# Give the script an offset indicating which run it should start on,
+# such that the different ec2 instances have different seeds.
+offset=$1
+
 for i in $(seq 1 50)
 do
     echo "*** This is run $i ****"
-    python -m linear_hf.experiment_100_nocausal $i
-    python -m linear_hf.experiment_100_causality $i
+    python -m linear_hf.experiment_100_nocausal $(($i+$offset))
+    python -m linear_hf.experiment_100_causality $(($i+$offset))
 done
