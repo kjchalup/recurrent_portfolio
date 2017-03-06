1badb936b905696ba318dfc7b326ac0e17a9a94c
Krzysztof Chalupka <kjchalupk@caltech.edu>
2017-03-05 17:00:35 -0800

    significantly simplified clean_data.py

--- linear_hf/clean_data.py
+++ linear_hf/clean_data.py
@@ -1,21 +1,19 @@
-import glob
-
 """ Clean data of 'NaN,NaN,NaN,99' for open, high, low, close data.
 
-    Replaces strings of 'NaN,NaN,NaN,99' to 'NaN,NaN,NaN,NaN'.
+    Replaces strings of 'NaN,NaN,NaN,99' to 'NaN,NaN,NaN,NaN'. The "99 pattern"
+    happens in CRSP data when a stock is delisted.
 """
+import os
+import glob
+DATA_DIR = 'tickerData'
 
-all_txt = glob.glob('tickerData/*.txt')
-questionable_files = []
-for fname in all_txt:
-
-    filedata = None
-    with open(fname, 'r') as file:
-        filedata = file.read()
-    f = open(fname, 'r').readlines()
-
-    if len([s for s in f if 'NaN,NaN,NaN,99' in s])>0:
-        # Replace the target string
+if __name__ == "__main__":
+    ALL_TXT = glob.glob(os.path.join(DATA_DIR, '*.txt'))
+    QUESTIONABLE_FILES = []
+    for fname in ALL_TXT:
+        filedata = None
+        with open(fname, 'r') as f:
+            filedata = f.read()
         filedata = filedata.replace('NaN,NaN,NaN,99', 'NaN,NaN,NaN,NaN')
-        with open(fname, 'w') as file:
-            file.write(filedata)
+        with open(fname, 'w') as f:
+            f.write(filedata)
