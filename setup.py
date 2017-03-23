4cfec93be7d165039363c224be73b2a5e748aec3
Tristan James McKinney <velociraptor@Tristans-MacBook-Pro.local>
2017-03-23 12:45:45 -0700

    Remove unused hooks and improve setup.py.

--- setup.py
+++ setup.py
@@ -1,15 +1,21 @@
+"""setup.py for the causehf repository."""
+import os
 from setuptools import setup
 
+def read(fname):
+    """Utiltiy function for README file."""
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
 setup(name='causehf',
-      # version=__version__,      
+      # version=__version__,
       description='Stock portfolio generation with recurrent neural networks.',
       # url='',
       # download_url='',
       author='Krzysztof Chalupka, Alex Teng, Tristan McKinney',
       author_email='krzysztof@cause.ai, alex@cause.ai, tristan@cause.ai',
       license='MIT',
-      # packages=[],
-      include_package_data = True,
+      packages=['linear_hf', ],
+      include_package_data=True,
       install_requires=[
           'pytest==3.0.7',
           'quantiacsToolbox==2.2.11',
@@ -18,4 +24,5 @@ setup(name='causehf',
           'tensorflow==1.0.1',
       ],
       zip_safe=False,
+      long_description=read('README.md'),
      )
